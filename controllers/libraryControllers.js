c5565e7f78703fa7698b2377c28ad63f123e9a8c
HarryAhnHS <hongsuhahn@gmail.com>
2024-08-19 03:53:32 +0100

    fix getFolder query + params

--- controllers/libraryControllers.js
+++ controllers/libraryControllers.js
@@ -3,14 +3,14 @@ const db = require('../db/queries');
 
 module.exports = {
     redirectToRoot: async (req, res) => {
-        console.log('testing', req.user.id);
         const rootFolder = await db.getRootFolder(req.user.id);
         res.redirect(`/library/${rootFolder.id}`);
     },
     renderFolder: async (req, res) => {
         const userId = req.user.id;
-        const folderId = req.params.id;
+        const folderId = req.params.folderId;
         const folder = await db.getFolder(userId, folderId);
+        console.log("populating with folder: ", folder);
         res.render('library', {
             folder: folder
         })
